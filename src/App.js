02f10ba146ba369a9c70ee23233d7f5d31a8e123
Eduardo Olguin <eddo555@hotmail.com>
2021-02-22 18:40:23 +0100

    array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "./Context";
 function App() {
-  const msg = useContext(Context);
-  return <div>{msg}</div>;
+  const { list } = useContext(Context);
+  return <div>{list}</div>;
 }
 
 export default App;
--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,20 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 const Context = createContext();
 
 function ContextProvider(props) {
-  return (
-    <Context.Provider value="hello world">{props.children}</Context.Provider>
-  );
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    let array = [];
+
+    for (let i = 0; i < 15; i++) {
+      array.push(i++);
+    }
+    array = array.sort(() => Math.random() - 0.5);
+    setList(array);
+  }, []);
+  return <Context.Provider value={{ list }}>{props.children}</Context.Provider>;
 }
 
 export { Context, ContextProvider };
